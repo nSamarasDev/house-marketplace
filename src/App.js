ab53540d3eaa10c815fb4f7d5d728c0a8faa4418
“[Nicholas <[n.samaras4@outlook.com]>
2022-01-28 13:49:53 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+//import PrivateRoute from './components/PrivateRoute'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+ 
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Explore />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+      </Routes>
+        <Navbar />
+    </Router>
+
+    <ToastContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,29 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+
+
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyB151_PNaf5Xn0LTKf8dOzJXg4ConDql8M",
+
+  authDomain: "house-marketplace-app-18d43.firebaseapp.com",
+
+  projectId: "house-marketplace-app-18d43",
+
+  storageBucket: "house-marketplace-app-18d43.appspot.com",
+
+  messagingSenderId: "858795671272",
+
+  appId: "1:858795671272:web:a91e60bb679d3c6cfe935d"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
