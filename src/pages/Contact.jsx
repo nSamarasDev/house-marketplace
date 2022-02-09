694e113505052fa96525c35caa5fa72862e039b5
“[Nicholas <[n.samaras4@outlook.com]>
2022-02-09 14:28:31 -0500

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,47 +1,74 @@
-import React from 'react';
-import {useState, useEffect} from 'react'
-import {useParams, useSearchParams} from 'react-router-dom'
-import {doc, getDoc} from 'firebase/firestore'
-import {db} from '../firebase.config'
-import {toast} from 'react-toastify'
-
+import { useState, useEffect } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
 
 function Contact() {
-    const [message, setMessage] = useState('')
-    const [landlord, setLandlord] = useState(null)
-    const [searchParams, setSearchParams] = useSearchParams()
+  const [message, setMessage] = useState('')
+  const [landlord, setLandlord] = useState(null)
+  // eslint-disable-next-line
+  const [searchParams, setSearchParams] = useSearchParams()
 
-    const params = useParams()
+  const params = useParams()
 
-    useEffect(() => {
-        const getLandlord = async () => {
-            const docRef = doc(db, 'users', params.landlord)
-            const docSnap = await getDoc(docRef)
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, 'users', params.landlordId)
+      const docSnap = await getDoc(docRef)
 
-            if(docSnap.exists()) {
-                setLandlord(docSnap.data())
-            } else {
-                toast.error('Could not retrive landlord data')
-            }
-        }
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data())
+      } else {
+        toast.error('Could not get landlord data')
+      }
+    }
 
-        getLandlord()
+    getLandlord()
+  }, [params.landlordId])
 
-    }, [params.landlordId])
+  const onChange = (e) => setMessage(e.target.value)
 
-  return <div className='pageContainer'>
+  return (
+    <div className='pageContainer'>
       <header>
-      <p className="pageHeader"></p>
+        <p className='pageHeader'>Contact Landlord</p>
       </header>
 
       {landlord !== null && (
-          <main>
-              <div className='contactLandlord'>
-                  <p className="landlordName">{landlord?.name}</p>
-              </div>
-          </main>
+        <main>
+          <div className='contactLandlord'>
+            <p className='landlordName'>Contact {landlord?.name}</p>
+          </div>
+
+          <form className='messageForm'>
+            <div className='messageDiv'>
+              <label htmlFor='message' className='messageLabel'>
+                Message
+              </label>
+              <textarea
+                name='message'
+                id='message'
+                className='textarea'
+                value={message}
+                onChange={onChange}
+              ></textarea>
+            </div>
+
+            <a
+              href={`mailto:${landlord.email}?Subject=${searchParams.get(
+                'listingName'
+              )}&body=${message}`}
+            >
+              <button type='button' className='primaryButton'>
+                Send Message
+              </button>
+            </a>
+          </form>
+        </main>
       )}
-  </div>;
+    </div>
+  )
 }
 
-export default Contact;
+export default Contact
