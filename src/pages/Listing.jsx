7b36e0a019b174f48456d8a08463a40a13559660
“[Nicholas <[n.samaras4@outlook.com]>
2022-02-10 13:58:02 -0500

    update

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useState, useEffect} from 'react'
 import {Link, useNavigate, useParams} from 'react-router-dom'
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
 import {getDoc, doc} from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 import {db} from '../firebase.config'
@@ -92,7 +93,21 @@ function Listing() {
 
             <p className="listingLocationTitle">Location</p>
 
-            {/* Map */}
+            <div className="leafletContainer">
+              <MapContainer style={{height: '100%', width: '100%', }}
+              center={[listing.geolocation.lat, listing.geolocation.lng]}
+               zoom={13} scrolllWheelZoom={false}
+               >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            />
+            <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+              <Popup>{listing.location}</Popup>
+
+            </Marker>
+              </MapContainer>
+            </div>
 
             {auth.currentUser?.uid !== listing.userRef && (
           <Link
